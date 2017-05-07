e91908bd8f6046854596b00fe692d4f2630bd094
sum-kcid <jayp1924@gmail.com>
2017-05-07 02:59:08 -0400

    updated todo

--- src/pompa.js
+++ src/pompa.js
@@ -1,16 +1,16 @@
 /* todo
+	* finish tests
 	* create route, route model & page dump configurators
 	* create radio between data source and collections
 		- create data source transformer (local or GET req for .csv & .json)
 	* create data source configurator
 	* create collection/data source API configurator
 		- update page generateHTML to add form handlers
-	* integrate Handlebars.js
-		- update PageContainer 'parseTemplate' method & Page generateHTML
 
 	* internal sanity and integrity validation checks
 	* api sanity checks + configurator
-	* unified error handling
+	// * unified error handling
+	// don't need
 
 	* make a boilerplate that runs off a config file - so users do not have to write any js
 		* general config file
@@ -22,8 +22,6 @@
 	* documentation website
 
 	* feedback and iterations!
-
-	* i hope modules use topological sorts lol cause i have interdependent shit rip
 */
 
 let MainClasses = require('./Woven.js');
